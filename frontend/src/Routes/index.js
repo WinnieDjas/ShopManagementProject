66d6637f63421436ead1e4140ae60621e315e37c
winnyh20 <winny.ihirwe@gmail.com>
2022-11-08 21:19:18 +0000

    frontend firstcommit

---
+++ frontend/src/Routes/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomeView from "../View/Home";
+import DashboardView from "../View/Dashboard";
+import ProductView from "../View/Product";
+import SalesView from "../View/Sales";
+import PurchaseView from "../View/Purchase";
+import UsersView from "../View/User";
+import DashRoutes from "./DashRoutes";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomeView />} />
+      <Route path="/login" element={<HomeView />} />
+      <Route element={<DashRoutes />}>
+        <Route path="/user" element={<UsersView />} />
+        <Route path="/dashboard" element={<DashboardView />} />
+        <Route path="/sales" element={<SalesView />} />
+        <Route path="/purchase" element={<PurchaseView />} />
+        <Route path="/product" element={<ProductView />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default MainRoutes;
